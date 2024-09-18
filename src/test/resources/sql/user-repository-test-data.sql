f4c33237468d5667ee6026328ec7baa95cc17d3e
ckr <ckr3453@gmail.com>
2024-09-18 18:04:45 +0900

    test: user repository 테스트용 sql 파일 추가

---
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,2 @@
+insert into users (id, email, nickname, address, certification_code, status, last_login_at)
+values (1, 'david3453@naver.com', 'ckr', 'Seoul, Gunja', '123123-123123-123-123-123123123', 'ACTIVE', 0);
