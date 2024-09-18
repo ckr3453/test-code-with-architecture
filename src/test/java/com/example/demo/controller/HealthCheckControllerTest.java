97d0f64f151ff531c41409e1503b9cbb7eb214d9
ckr <ckr3453@gmail.com>
2024-09-18 19:47:11 +0900

    test: HealthCheckController 테스트 코드 추가

---
+++ src/test/java/com/example/demo/controller/HealthCheckControllerTest.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * packageName : com.example.demo.controller
+ * fileName    : HealthCheckControllerTest
+ * author      : ckr
+ * date        : 24. 9. 18.
+ * description :
+ */
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+class HealthCheckControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void health_check_응답이_200_으로_내려온다() throws Exception {
+        //given
+        //when
+        //then
+        mockMvc.perform(get("/health_check.html"))
+            .andExpect(status().isOk());
+    }
+}
