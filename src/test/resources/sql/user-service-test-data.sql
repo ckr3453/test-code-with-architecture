d908c71c002d0b18488d8fdd56f8803d4f6bccd5
ckr <ckr3453@gmail.com>
2024-09-18 18:21:31 +0900

    test: user service 테스트용 sql 파일 및 코드 추가

---
+++ src/test/resources/sql/user-service-test-data.sql
@@ -0,0 +1,5 @@
+insert into users (id, email, nickname, address, certification_code, status, last_login_at)
+values (1, 'david3453@naver.com', 'ckr', 'Seoul, Gunja', '123123-123123-123-123-123123123', 'ACTIVE', 0);
+
+insert into users (id, email, nickname, address, certification_code, status, last_login_at)
+values (2, 'david3454@naver.com', 'ckr2', 'Seoul, Gunja', '123123-123123-123-123-123123123', 'PENDING', 0);
