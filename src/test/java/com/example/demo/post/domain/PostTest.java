cb0d271b79176d60f910b084fbe024cef6288d2b
ckr <ckr3453@gmail.com>
2024-09-20 17:47:01 +0900

    test: Post 도메인 테스트 코드 추가

--- src/test/java/com/example/demo/post/domain/PostTest.java
+++ src/test/java/com/example/demo/post/domain/PostTest.java
@@ -1,6 +1,10 @@
 package com.example.demo.post.domain;
 
+import com.example.demo.user.domain.User;
+import com.example.demo.user.domain.UserStatus;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * packageName : com.example.demo.post.domain
@@ -14,7 +18,35 @@ class PostTest {
     @Test
     void PostCreate_로_게시물을_만들_수_있다() {
         // given
+        long writerId = 1L;
+        String writerEmail = "test@test.com";
+        String writerNickname = "test";
+        String writerAddress = "Busan";
+        String writerCertificationCode = "123123-123-123-123123123";
+        String newContent = "hello. test";
+
+        PostCreate postCreate = PostCreate.builder()
+            .writerId(writerId)
+            .content(newContent)
+            .build();
+        User user = User.builder()
+            .id(writerId)
+            .email(writerEmail)
+            .nickname(writerNickname)
+            .address(writerAddress)
+            .status(UserStatus.ACTIVE)
+            .certificationCode(writerCertificationCode)
+            .build();
+
         // when
+        Post post = Post.from(user, postCreate);
+
         // then
+        assertThat(post.getContent()).isEqualTo(newContent);
+        assertThat(post.getWriter().getEmail()).isEqualTo(writerEmail);
+        assertThat(post.getWriter().getNickname()).isEqualTo(writerNickname);
+        assertThat(post.getWriter().getAddress()).isEqualTo(writerAddress);
+        assertThat(post.getWriter().getStatus()).isEqualTo(UserStatus.ACTIVE);
+        assertThat(post.getWriter().getCertificationCode()).isEqualTo(writerCertificationCode);
     }
 }
