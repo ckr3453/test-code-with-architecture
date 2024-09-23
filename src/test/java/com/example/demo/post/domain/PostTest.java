2853000c08332932a67f99b0f7b51a57597417fa
ckr <ckr3453@gmail.com>
2024-09-23 17:34:20 +0900

    test: ClockHolder 주입으로 변경됨에 따라 TestClockHolder 객체로 생성날짜 테스트 추가, 게시물 수정에 대한 테스트 코드 추가

--- src/test/java/com/example/demo/post/domain/PostTest.java
+++ src/test/java/com/example/demo/post/domain/PostTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.post.domain;
 
+import com.example.demo.mock.TestClockHolder;
 import com.example.demo.user.domain.User;
 import com.example.demo.user.domain.UserStatus;
 import org.junit.jupiter.api.Test;
@@ -29,7 +30,7 @@ class PostTest {
             .writerId(writerId)
             .content(newContent)
             .build();
-        User user = User.builder()
+        User writer = User.builder()
             .id(writerId)
             .email(writerEmail)
             .nickname(writerNickname)
@@ -39,14 +40,57 @@ class PostTest {
             .build();
 
         // when
-        Post post = Post.from(user, postCreate);
+        long createdAt = 123456789L;
+        Post post = Post.from(writer, postCreate, new TestClockHolder(createdAt));
 
         // then
         assertThat(post.getContent()).isEqualTo(newContent);
+        assertThat(post.getCreatedAt()).isEqualTo(createdAt);
         assertThat(post.getWriter().getEmail()).isEqualTo(writerEmail);
         assertThat(post.getWriter().getNickname()).isEqualTo(writerNickname);
         assertThat(post.getWriter().getAddress()).isEqualTo(writerAddress);
         assertThat(post.getWriter().getStatus()).isEqualTo(UserStatus.ACTIVE);
         assertThat(post.getWriter().getCertificationCode()).isEqualTo(writerCertificationCode);
+    }
+
+    @Test
+    void PostUpdate_로_게시물을_수정할_수_있다() {
+        // given
+        long writerId = 1L;
+        String writerEmail = "test@test.com";
+        String writerNickname = "test";
+        String writerAddress = "Busan";
+        String writerCertificationCode = "123123-123-123-123123123";
+        String oldContent = "hello. test";
+
+        User writer = User.builder()
+            .id(writerId)
+            .email(writerEmail)
+            .nickname(writerNickname)
+            .address(writerAddress)
+            .status(UserStatus.ACTIVE)
+            .certificationCode(writerCertificationCode)
+            .build();
+
+        Post post = Post.builder()
+            .id(1L)
+            .content(oldContent)
+            .createdAt(1L)
+            .modifiedAt(0L)
+            .writer(writer)
+            .build();
+
+        String newContent = "hello. test??";
+        PostUpdate postUpdate = PostUpdate.builder()
+            .content(newContent)
+            .build();
+
+        // when
+        long modifiedAt = 123456789L;
+        Post updatedPost = post.update(postUpdate, new TestClockHolder(modifiedAt));
+
+        // then
+        assertThat(updatedPost.getContent()).isEqualTo(newContent);
+        assertThat(updatedPost.getModifiedAt()).isEqualTo(modifiedAt);
     }
 }
